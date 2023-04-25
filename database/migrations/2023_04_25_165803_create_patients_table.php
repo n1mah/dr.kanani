9a4234955b22251ef860d0c4f8c9069e18ccc0f1
n1mah <nima.heidari.n@gmail.com>
2023-04-25 22:01:25 +0330

    add migration for patients & insurances TBLs

---
+++ database/migrations/2023_04_25_165803_create_patients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id("national_code");
+            $table->string("firstname");
+            $table->string("lastname");
+            $table->timestamp("birthdate");
+            $table->unsignedBigInteger("insurance_id");
+            $table->foreign("insurance_id")->references("id")->on("insurances")->onUpdate("cascade")->onDelete("cascade");
+            $table->string("mobile");
+            $table->string("phone")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
