4b15076accc591eff0a00fb4fe961542072f7b03
n1mah <nima.heidari.n@gmail.com>
2023-04-26 10:36:35 +0330

    add panel admin view & base styles

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset("fonts/style.css")}}">
+    <link rel="stylesheet" href="{{asset("admin/css/app.css")}}">
+</head>
+<body>
+    <div id="main">
+        <aside>
+            <header>
+                <h1>پنل مدیریت</h1>
+            </header>
+            <hr>
+            <nav>
+                <ul>
+                    <li class="active"><a href="{{url("#")}}">داشبورد</a></li>
+                    <li><a href="{{url("#")}}">بیمار</a></li>
+                    <li><a href="{{url("#")}}">بیمه</a></li>
+                </ul>
+            </nav>
+        </aside>
+        <div class="body">
+            <header>
+
+            </header>
+            <hr>
+            <div>
+                <button>See</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
