308e48f52715b6e7b8a4331d5c70be0fd77e9d0b
n1mah <nima.heidari.n@gmail.com>
2023-04-29 10:53:27 +0330

    Change Patient Add Form and show Error Box

--- resources/views/admin/patient-add.blade.php
+++ resources/views/admin/patient-add.blade.php
@@ -10,50 +10,84 @@
                 <br>
                 <form action="{{route("patient.store")}}" method="post">
                     @csrf
+                    @method('post')
                     <div>
-                        <label for="national_code">کدملی</label>
-                        <input type="number" id="national_code">
+                        <label for="national_code">کدملی <span class="star-red">*</span></label>
+                        <input type="number" id="national_code" name="national_code">
                     </div>
                     <br>
                     <div>
-                        <label for="fName">نام</label>
-                        <input type="text" id="fName">
+                        <label for="fName">نام<span class="star-red">*</span></label>
+                        <input type="text" id="fName" name="firstname">
                     </div>
                     <br>
                     <div>
-                    <label for="lName">نام خانوادگی</label>
-                    <input type="text" id="lName">
+                    <label for="lName">نام خانوادگی<span class="star-red">*</span></label>
+                    <input type="text" id="lName" name="lastname">
                     </div>
                     <br>
                     <div>
-                    <label for="birth">تولد</label>
-                    <input type="text" id="birth">
+                    <label>تولد<span class="star-red">*</span></label>
+                        <div>
+                            <label for="year">سال</label>
+                            <select id="year" name="year">
+                                @for($i = 1300; $i <= 1402; $i++)
+                                <option value="{{$i}}">{{$i}}</option>
+                                @endfor
+                            </select>
+                        </div>
+                        <div>
+                            <label for="month">ماه</label>
+                            <select id="month" name="month">
+                                @for($i = 1; $i <= 12; $i++)
+                                <option value="{{$i}}">{{$i}}</option>
+                                @endfor
+                            </select>
+                        </div>
+                        <div>
+                            <label for="day">روز</label>
+                            <select id="day" name="day">
+                                @for($i = 1; $i <= 31; $i++)
+                                <option value="{{$i}}">{{$i}}</option>
+                                @endfor
+                            </select>
+                        </div>
                     </div>
                     <br>
                     <div>
-                    <label for="insurance">نوع بیمه</label>
-                    <select id="insurance">
-                        <option value="">تامین اجتماعی</option>
-                        <option value="">آزاد</option>
-                    </select>
+                    <label for="insurance">نوع بیمه<span class="star-red">*</span></label>
+                        <select id="insurance" name="insurance_id">
+                            @foreach($insurances as $insurance)
+                            <option value="{{$insurance->id}}">{{$insurance->title}}</option>
+                            @endforeach
+
+                        </select>
+
                     </div>
                     <br>
                     <div>
-                    <label for="mobile">موبایل</label>
-                    <input type="number" id="mobile">
+                    <label for="mobile">موبایل<span class="star-red">*</span></label>
+                    <input type="number" id="mobile" name="mobile">
                     </div>
                     <br>
                     <div>
                     <label for="phone">تلفن ثابت</label>
-                    <input type="number" id="phone">
+                    <input type="number" id="phone" name="phone">
                     </div>
                     <br>
-                    <input class="btn" type="button" value="افزودن">
+                    <input class="btn" type="submit" value="افزودن">
                 </form>
                 <br>
                 <div class="back-box">
                     <a href="{{route("patients")}}">بازگشت</a>
                 </div>
+                @if($errors->any())
+                    <div class="errorBox">
+                        @foreach($errors->all() as $error)
+                            <strong>- {{ $error }}</strong>
+                        @endforeach
+                    </div>
+                @endif
             </div>
         </div>
     </div>
