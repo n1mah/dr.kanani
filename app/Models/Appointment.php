000b5f6080525200246523ad029b3efbcab7ad07
n1mah <nima.heidari.n@gmail.com>
2023-05-08 00:41:11 +0330

    Revert "commit jallali date"
    This reverts commit 3722fc2863f7528543bb5887eab352e1168ff8fe.

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -5,11 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Mokhosh\Jarbon\JarbonTrait;
 
 class Appointment extends Model
 {
-    use JarbonTrait;
     use HasFactory;
     public function patient(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
@@ -24,21 +22,13 @@ class Appointment extends Model
     ];
     protected $primaryKey = 'id';
 
-//    protected function visitTimeJ(): Attribute
-//    {
-//        return Attribute::make(
-//            get: fn() => $this->visit_time, //1402
-////            set: fn () => date('Y-m-d H:i:s', $value),
-//        );
-//    }
-     protected function visitTime(): Attribute
+    protected function visitTime(): Attribute
     {
         return Attribute::make(
-            get: fn ($value) =>  $value->jormat("y"), //1402
-//            set: fn ($value) => date('Y-m-d H:i:s', $value),
+            get: fn ($value) =>  (strtotime($value))-(60*60*1), //1402
+            set: fn ($value) => date('Y-m-d H:i:s', $value),
         );
     }
-
     protected function changeStatus(): Attribute
     {
         return Attribute::make(
--- config/jarbon.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-
-    'default_format' => 'Y-m-d H:i:s',
-    'convert_numbers' => false,
-
-];
